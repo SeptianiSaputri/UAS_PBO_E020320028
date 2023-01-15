bae7b96b5664bb08aefe723c2297fa002608e812
SeptianiSaputri <septianisaputri77@gmail.com>
2023-01-15 19:24:42 +0700

    first commit

---
+++ FDA_E020320028/app/src/main/java/id/ac/poliban/va/septi/fda_e020320028/Interface/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package id.ac.poliban.va.septi.fda_e020320028.Interface;
+
+public interface ChangeNumberItemsListener {
+    void changed();
+}
---
+++ Project82E020320028/app/src/main/java/id/ac/poliban/mi/va/septi/project82e020320028/MainActivity.java
@@ -0,0 +1,14 @@
+package id.ac.poliban.mi.va.septi.project82e020320028;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
